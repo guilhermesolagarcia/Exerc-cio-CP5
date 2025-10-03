6d04fd2b441d421ad4f0467e6494440ac309c002
PedroSPequini <ppequini2006@gmail.com>
2025-10-03 10:49:29 -0300

    Ajustando a Main.tsx.

--- cp5-projeto/src/main.tsx
+++ cp5-projeto/src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from './App.tsx';
+import { Login } from '../routes/Login/index.tsx'
+import { Cadastro } from '../routes/Cadastro/index.tsx';
+
 import './globals.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+        children: [ 
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/cadastro",
+        element: <Cadastro />,
+      },
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
