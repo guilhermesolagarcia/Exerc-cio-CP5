e997e13db2d831124220b5e10caad757f98ddbef
Claytonasantos <claytonalves2579@gmail.com>
2025-10-03 21:22:45 -0300

    Criação dentro da pasta Routes a pasta Home e o arquivo index tsx

---
+++ cp5-projeto/routes/Home/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Bem-vindo(a)! Login realizado com sucesso.</h1>
+    </div>
+  );
+}
